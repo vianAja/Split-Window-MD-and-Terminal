4f93813d70950eec88b4ee0edf3c96fefbe79d06
vianAja <najwanoctavian@gmail.com>
2025-09-15 23:05:18 +0700

    update _app.js to disable direct / to /login

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,14 +4,5 @@ import "../styles/globals.css";
 import "@xterm/xterm/css/xterm.css";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Kalau user buka root "/", arahkan ke /login
-    if (router.pathname === "/") {
-      router.replace("/login");
-    }
-  }, [router]);
-
   return <Component {...pageProps} />;
 }
